af26a5a75e29fda2921586083d38d311689c860c
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2020-01-08 18:36:48 -0800

    Tag S3 buckets with their own name

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@
 source "https://rubygems.org"
 
 gem "aws-sdk-datapipeline"
+gem "aws-sdk-s3"
 gem "aws-sdk-ec2"
 gem "pry"
---
+++ s3.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "aws-sdk-s3"
+
+class S3
+  def tag_buckets_with_bucket_name
+    bucket_names.each do |name|
+      client.put_bucket_tagging({
+        bucket: name,
+        tagging: {
+          tag_set: [
+            {
+              key: "s3_bucket_name",
+              value: name,
+            },
+          ],
+        },
+      })
+    end
+  end
+
+  private
+
+  def bucket_names
+    client.list_buckets.each_page.flat_map(&:buckets).map(&:name)
+  end
+
+  def client
+    @client ||= Aws::S3::Client.new
+  end
+end
